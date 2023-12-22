2628eeeed3385988ed1df767850e73fb6b91df32
Maël Ehrhard <mael.ehrhard@outlook.fr>
2023-12-22 14:13:23 +0100

    ✨ Add signIn route

--- BACKEND/src/auth/auth.controller.ts
+++ BACKEND/src/auth/auth.controller.ts
@@ -1,9 +1,12 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { LoginDto } from './login.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
-  @Post()
-  signIn() {}
+  @Post('signIn')
+  signIn(@Body() loginDto: LoginDto) {
+    return this.authService.signIn(loginDto.login, loginDto.password);
+  }
 }
