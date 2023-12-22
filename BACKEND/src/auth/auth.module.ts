ba1b8b708bf2991c2c0a3e6b7e29c727132b9cb3
Maël Ehrhard <mael.ehrhard@outlook.fr>
2023-12-22 14:33:53 +0100

    🛂 Add JWT

--- BACKEND/src/auth/auth.module.ts
+++ BACKEND/src/auth/auth.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    JwtModule.registerAsync({
+      useFactory: (config: ConfigService) => ({
+        global: true,
+        secret: config.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '365d' },
+      }),
+      imports: [ConfigModule],
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- BACKEND/src/auth/auth.service.ts
+++ BACKEND/src/auth/auth.service.ts
@@ -1,15 +1,19 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   async signIn(login: string, password_: string) {
     const user = await this.usersService.findOne(login);
     if (user?.password === password_) {
-      delete user.password;
-      return user;
+      const payload = { id: user.id, login: user.login };
+      return { access_token: await this.jwtService.signAsync(payload) };
     }
     throw new UnauthorizedException();
   }
