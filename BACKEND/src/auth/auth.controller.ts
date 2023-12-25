0e7729135bf34ec0dda5e7ff581f93092f693f5b
Maël Ehrhard <mael.ehrhard@outlook.fr>
2023-12-25 23:42:27 +0100

    ✨ Log user on successful register

--- BACKEND/src/auth/auth.controller.ts
+++ BACKEND/src/auth/auth.controller.ts
@@ -15,10 +15,13 @@ export class AuthController {
 
   @Public()
   @Post('/register')
-  register(@Body() registerDto: RegisterDto) {
+  async register(@Body() registerDto: RegisterDto) {
     if (registerDto.password !== registerDto.confirmation) {
       throw new BadRequestException("Password and confirmation don't match");
     }
-    return this.authService.register(registerDto);
+    const user = await this.authService.register(registerDto);
+    if (user) {
+      return this.authService.signIn(user.login, user.password);
+    }
   }
 }
