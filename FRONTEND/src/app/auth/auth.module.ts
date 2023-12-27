be3855108a4ee8bf2212f111c6e58d3a337319c0
Maël Ehrhard <mael.ehrhard@outlook.fr>
2023-12-27 19:15:57 +0100

    🚧 Register form WIP

--- FRONTEND/src/app/auth/auth.module.ts
+++ FRONTEND/src/app/auth/auth.module.ts
@@ -4,9 +4,15 @@ import { SharedModule } from '../shared/shared.module';
 import { RouterModule } from '@angular/router';
 import { authRoutes } from './auth-routes.constant';
 import { RegisterComponent } from './register/register.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [RegisterComponent],
-  imports: [CommonModule, SharedModule, RouterModule.forChild(authRoutes)],
+  imports: [
+    CommonModule,
+    SharedModule,
+    RouterModule.forChild(authRoutes),
+    ReactiveFormsModule,
+  ],
 })
 export class AuthModule {}
--- FRONTEND/src/app/auth/register/register.component.ts
+++ FRONTEND/src/app/auth/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RegisterDto } from './register.dto';
 import { AuthService } from '../../shared/services/auth.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -8,14 +9,38 @@ import { AuthService } from '../../shared/services/auth.service';
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
+  registerForm: FormGroup;
   registerDto: RegisterDto;
 
-  constructor(private readonly authService: AuthService) {
+  constructor(
+    private readonly authService: AuthService,
+    private readonly formBuilder: FormBuilder,
+  ) {
     this.registerDto = new RegisterDto();
+    this.registerForm = this.formBuilder.group({
+      firstname: ['', Validators.required],
+      lastname: ['', Validators.required],
+      address: ['', Validators.required],
+      postCode: [
+        '',
+        [Validators.required, Validators.minLength(5), Validators.maxLength(5)],
+      ],
+      city: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      sex: [
+        '',
+        [Validators.required, Validators.minLength(1), Validators.maxLength(1)],
+      ],
+      login: ['', Validators.required],
+      password: ['', Validators.required],
+      confirmation: ['', Validators.required],
+      telephone: ['', Validators.required],
+    });
   }
 
   onRegister() {
-    console.log(this.registerDto);
-    this.authService.register(this.registerDto).subscribe();
+    if (this.registerForm.valid) {
+      this.authService.register(this.registerDto).subscribe();
+    }
   }
 }
